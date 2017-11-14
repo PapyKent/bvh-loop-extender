fef42de5b816fccf43d45e1bf6a6550d845b9945
PapyKent <kentdu38@gmail.com>
2017-11-14 15:47:35 +0900

    wrap function with number of loops

--- mixamo-loop-extender.py
+++ mixamo-loop-extender.py
@@ -21,37 +21,35 @@ import Quaternions as Quaternions
 
 file_name = "mixamo"
 file_path= "./data/animations/"
+file = file_path+file_name+".bvh"
+
+def loop_extender(ext_number, file):
+    anim, names, frametime = BVH.load(file_path+file_name+".bvh")
+    frames_number = anim.positions.shape[0]
+    new_frames_number = frames_number * ext_number
+    elements_number =  anim.positions.shape[1]
+    new_positions = np.zeros((frames_number*ext_number, elements_number, anim.positions.shape[2]))
+    new_rotations = anim.rotations.copy()
+    for i in range(0,ext_number-1):
+        new_rotations.qs = np.vstack((new_rotations.qs, anim.rotations.qs))
 
 
-file_object = open(file_path+file_name+"_footsteps.txt", 'w')
-
-anim, names, frametime = BVH.load(file_path+file_name+".bvh")
-frames_number = len(anim)
-
-new_positions = np.zeros((anim.positions.shape[0]*2, anim.positions.shape[1], anim.positions.shape[2]))
-new_rotations = anim.rotations.copy()
-new_rotations.qs = np.vstack((new_rotations.qs, anim.rotations.qs))
-
-
-for i in range(0,76-1):
-    for j in range (0, 55-1):       
-        if i >= 37 :
-            new_positions[i][j] = anim.positions[i-37][j]+(0,0,anim.positions[36][j][2])         
-            new_rotations.qs[i][j] = anim.rotations.qs[i-37][j]
-        else:
-            new_positions[i][j] = anim.positions[i][j]
-            new_rotations.qs[i][j] = anim.rotations.qs[i][j]
+    for i in range(0,new_frames_number-1):
+        for j in range (0, elements_number-1):       
+            if i >= frames_number :
+                new_positions[i][j] = anim.positions[i%frames_number][j]+(0,0,new_positions[int(i/frames_number)*frames_number-1][j][2])         
+                new_rotations.qs[i][j] = anim.rotations.qs[i%frames_number][j]
+            else:
+                new_positions[i][j] = anim.positions[i][j]
+                new_rotations.qs[i][j] = anim.rotations.qs[i][j]
            
-  
-print(new_rotations.qs)
-print("////////////////////////////////")
-print(anim.rotations.qs)  
-       
 
-new_anim = Animation.Animation(new_rotations, new_positions, anim.orients, anim.offsets, anim.parents)
+    new_anim = Animation.Animation(new_rotations, new_positions, anim.orients, anim.offsets, anim.parents)
 
+    BVH.save(file_path+"test.bvh", new_anim, names, frametime)
+    
+    return;
 
-
-BVH.save(file_path+"test.bvh", new_anim, names, frametime)
+loop_extender(10,file)
 
 
